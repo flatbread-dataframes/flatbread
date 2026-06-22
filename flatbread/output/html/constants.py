56e7dfed6701de0ce2ead7198b899e783c2d1e25
lcvriend <43742775+lcvriend@users.noreply.github.com>
2026-06-22 21:28:50 +0200

    chore: bump flabread-table version

--- flatbread/output/html/constants.py
+++ flatbread/output/html/constants.py
@@ -1,4 +1,4 @@
-FLATBREAD_TABLE_VERSION = "v0.1.1"
+FLATBREAD_TABLE_VERSION = "v0.1.2"
 FLATBREAD_TABLE_URL = (
     "https://cdn.jsdelivr.net/gh/flatbread-dataframes/"
     f"flatbread-wc-table-display@{FLATBREAD_TABLE_VERSION}/src/viewer.js"
